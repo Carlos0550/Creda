e9534504b0eb7abe80dc85bf7ebbe9ff3aa33d4a
carlos0550 <carlospelinski03@gmail.com>
2025-03-31 15:57:40 -0300

    feat(): agrega funcion para obtener el usuario

--- frontend/src/Context/useUsers.tsx
+++ frontend/src/Context/useUsers.tsx
@@ -7,38 +7,39 @@ function useUsers() {
         const url = new URL(globalApis.users + "/create-user")
         console.log(url)
         try {
-            const result = await fetch(url,{
+            const result = await fetch(url, {
                 method: "POST",
-                headers:{
+                headers: {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify(userData)
             })
 
             const responseData = await result.json()
-            if(!result.ok){ showNotification({
-                title:"No fue posible crear su cuenta",
-                message: responseData.msg || "Error desconocido",
-                position: "top-right",
-                autoClose: 3500,
-                color: "red"
-            })
-            return false
-        }
+            if (!result.ok) {
+                showNotification({
+                    title: "No fue posible crear su cuenta",
+                    message: responseData.msg || "Error desconocido",
+                    position: "top-right",
+                    autoClose: 3500,
+                    color: "red"
+                })
+                return false
+            }
 
             showNotification({
-                title:"Cuenta creada con éxito",
+                title: "Cuenta creada con éxito",
                 message: responseData.msg,
                 position: "top-right",
                 autoClose: 3500,
-                color: "green" 
+                color: "green"
             })
 
             return true
         } catch (error) {
             console.log(error)
             showNotification({
-                title:"No fue posible crear su cuenta",
+                title: "No fue posible crear su cuenta",
                 message: error.message || "Error desconocido",
                 position: "top-right",
                 autoClose: 5000,
@@ -47,21 +48,21 @@ function useUsers() {
 
             return false;
         };
-    },[]);
+    }, []);
 
     const loginUser = useCallback(async (userData: any) => {
         const url = new URL(globalApis.users + "/login-user")
         try {
             const response = await fetch(url, {
                 method: "POST",
-                headers:{
+                headers: {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify(userData)
             });
 
             const responseData = await response.json()
-            if(!response.ok){
+            if (!response.ok) {
                 showNotification({
                     title: "Error al iniciar sesión",
                     message: responseData.msg || "Error desconocido.",
@@ -79,9 +80,10 @@ function useUsers() {
                 autoClose: 2500,
                 color: "blue"
             })
-
+            const respondeUserData = responseData.userData
+            localStorage.setItem("user_data", JSON.stringify(respondeUserData))
             return true
-            
+
         } catch (error) {
             console.log(error)
             showNotification({
@@ -94,13 +96,30 @@ function useUsers() {
 
             return false
         }
-    },[])
-  
-    return useMemo(()=> ({
-        createUser, loginUser
-  }),[
-        createUser, loginUser
-  ])
+    }, [])
+
+    const getLocaleUserInfo = useCallback(() => {
+        const locale_user = localStorage.getItem("user_data")
+        if (!locale_user) {
+            showNotification({
+                title: "No se encontraron datos del usuario.",
+                message: "Intente iniciar sesion nuevamente,",
+                autoClose: 3500,
+                position: "top-right",
+                color: "yellow"
+            })
+
+            return undefined
+        }
+        const user_info = JSON.parse(locale_user)
+        return user_info
+    }, [loginUser])
+
+    return useMemo(() => ({
+        createUser, loginUser, getLocaleUserInfo
+    }), [
+        createUser, loginUser, getLocaleUserInfo
+    ])
 }
 
 export default useUsers
