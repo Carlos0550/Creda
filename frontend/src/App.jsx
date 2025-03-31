7fac8f2207431f824390f0df812cf0932b23d78b
carlos0550 <carlospelinski03@gmail.com>
2025-03-31 15:56:40 -0300

    feat(): Agrega una ruta para un dashboard/home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,14 @@ import { Routes, Route } from "react-router-dom"
 import "./App.css"
 
 import AuthComponentManager from './Authentication/AuthComponentManager'
+import Home from './Home/Home'
+import Layout from './Layout/Layout'
 function App() {
   return (
     <div className='main-container'>
       <Routes>
         <Route path='/' element={<AuthComponentManager />} />
+        <Route path='/home/:user_id' element={<Layout content={<Home/>}/>}/>
       </Routes>
     </div>
   )
