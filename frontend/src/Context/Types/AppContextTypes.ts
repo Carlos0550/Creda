cc6f5508eafe041d790cbb60e96fd9b6978282ca
carlos0550 <carlospelinski03@gmail.com>
2025-03-31 15:57:48 -0300

    feat(): agrega tipado

--- frontend/src/Context/Types/AppContextTypes.ts
+++ frontend/src/Context/Types/AppContextTypes.ts
@@ -1,6 +1,10 @@
+import { LocaleUserTypes } from "./UserTypes";
+
+
 interface usersHookInterface{
     createUser: (userData: any) => Promise<boolean>;
     loginUser: (userData: any) => Promise<boolean>;
+    getLocaleUserInfo: () => Partial<Record<keyof LocaleUserTypes, string>>;
 }
 
 export interface AppContextValuesInterface{
---
+++ frontend/src/Context/Types/UserTypes.ts
@@ -0,0 +1,5 @@
+export interface LocaleUserTypes {
+    user_id: string;
+    user_name: string;
+    user_email: string;
+}
