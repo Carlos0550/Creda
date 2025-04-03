3a4f2e9ec670a4da92013343ddfc3d70bf984bab
carlos0550 <carlospelinski03@gmail.com>
2025-04-02 22:39:57 -0300

    Feat: Agrega nuevas apis

--- frontend/src/Context/APIs.ts
+++ frontend/src/Context/APIs.ts
@@ -1,4 +1,5 @@
 export const base_url_server = new URL("http://localhost:5000")
 export const globalApis = {
-    users: new URL(base_url_server + "users")
+    users: new URL(base_url_server + "users"),
+    predict: new URL(base_url_server + "predict"),
 }
