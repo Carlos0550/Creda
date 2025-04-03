06ee13c44db5f5b4a17b91e9363a237fcdd6dc0d
carlos0550 <carlospelinski03@gmail.com>
2025-04-02 22:44:57 -0300

    feat: seccion principal

--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import "./Home.css"
+import FileUploader from './Components/FIleUploader/FileUploader'
 function Home() {
   return (
     <React.Fragment>
-        <div>
-      
+        <div className='home-container'>
+          <div className='operations-register-container'>
+              <h3>Registro de predicciones</h3>
+              <p>Por el momento no hay ningún registro.</p>
+          </div>
+
+          <div className='new-operation-container'>
+              <FileUploader/>
+          </div>
         </div>
     </React.Fragment>
   )
