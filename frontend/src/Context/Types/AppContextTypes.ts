588e9ca2613f01b1e600819a5f112786ac776fc0
carlos0550 <carlospelinski03@gmail.com>
2025-04-02 22:44:27 -0300

    feat: tipado para el contexto global

--- frontend/src/Context/Types/AppContextTypes.ts
+++ frontend/src/Context/Types/AppContextTypes.ts
@@ -7,7 +7,13 @@ interface usersHookInterface{
     getLocaleUserInfo: () => Partial<Record<keyof LocaleUserTypes, string>>;
 }
 
+interface usePredictHookInterface{
+    uploading: boolean;
+    sendFile: (file: File) => Promise<boolean>;
+}
+
 export interface AppContextValuesInterface{
-    width: number,
-    usersHook: usersHookInterface,
+    width: number;
+    usersHook: usersHookInterface;
+    usePredictHook: usePredictHookInterface;
 }
