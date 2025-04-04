6ee453d1f54ab953ef0cf5021640a02b3b231428
carlos0550 <carlospelinski03@gmail.com>
2025-04-04 11:46:53 -0300

    feat: agrega tipado nuevo

--- backend/src/Types/predict.types.ts
+++ backend/src/Types/predict.types.ts
@@ -3,3 +3,8 @@ import { createUserFunctionInterface } from "./users.types";
 export interface PredicRequestEndpointInterface{
     user_data: Partial<createUserFunctionInterface>;
 }
+
+export interface MarkAsAnalyzedEndpointInterface{
+    columns: [];
+    fileName: string;
+}
