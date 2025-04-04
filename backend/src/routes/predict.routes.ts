fafbdd8b992002bcc9158ac2c74b80d65f8d439f
carlos0550 <carlospelinski03@gmail.com>
2025-04-04 11:46:38 -0300

    feat: nuevos endpoints y validaciones agregados

--- backend/src/routes/predict.routes.ts
+++ backend/src/routes/predict.routes.ts
@@ -3,9 +3,9 @@ import path from "path";
 import fs from "fs";
 import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
-import { PredicRequestEndpointInterface } from "../Types/predict.types";
-import { analyzeFile } from "../controllers/PredictionsController/predict.controller";
+import { getAndLockPendingFile, markFilesAsAnalyzed, saveFile, verifyFileState } from "../controllers/PredictionsController/predict.controller";
 import { createUserFunctionInterface } from "../Types/users.types";
+import { MarkAsAnalyzedEndpointInterface } from "../Types/predict.types";
 
 const predictRouter = Router();
 const uploadFolder = path.join(__dirname, "../uploads");
@@ -16,11 +16,11 @@ if (!fs.existsSync(uploadFolder)) {
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, uploadFolder); 
+    cb(null, uploadFolder);
   },
   filename: function (req, file, cb) {
-    
-    const uniqueFileName = `${uuidv4().slice(0,12)}${path.extname(file.originalname)}`;
+
+    const uniqueFileName = `${uuidv4().slice(0, 12)}${path.extname(file.originalname)}`;
     cb(null, uniqueFileName);
   },
 });
@@ -44,16 +44,66 @@ const verifyRequest = (
   next();
 };
 
-predictRouter.post(
-  "/analyze-file",
-  upload.single("file"), 
-  verifyRequest,            
-  (req: Request, res: Response) => {
-    res.status(200).json({
-      msg: "Archivo recibido y datos del usuario verificados.",
-      archivo: req.file?.filename,
+predictRouter.post("/analyze-file", upload.single("file"), verifyRequest, (req: Request, res: Response, next: NextFunction) => {
+  
+  res.status(200).json({
+    msg: "Archivo recibido y datos del usuario verificados.",
+    archivo: req.file?.filename,
+  });
+  next()
+}, saveFile);
+
+predictRouter.get("/file-status", async (req: Request, res: Response, next: NextFunction) => {
+  const { status, user_id } = req.query
+  if (!status) {
+    res.status(400).json({
+      msg: "El parametro 'status' no se encontró en la solicitud."
+    })
+    return
+  }
+
+  if(!user_id){
+    res.status(400).json({
+      msg: "No se encontro el ID del usuario en la solicitud."
+    })
+  }
+  const validStatuses = ["pending", "locked", "analyzed"] as const;
+  type FileStatus = typeof validStatuses[number]
+  if (!status || typeof status !== "string") {
+    res.status(400).json({ msg: "Falta o es inválido el parámetro 'status'" });
+    return
+  }
+
+  if (!validStatuses.includes(status as FileStatus)) {
+    res.status(400).json({
+      msg: `Estado inválido. Usa uno de: ${validStatuses.join(", ")}`
     });
-  }, analyzeFile
-);
+    return
+  }
+
+  next()
+}, verifyFileState)
+
+predictRouter.post("/get-pending", getAndLockPendingFile)
+
+predictRouter.post("/mark-as-analyzed", async(req:Request<{}, {}, MarkAsAnalyzedEndpointInterface>, res:Response, next:NextFunction) => {
+  const { fileName, columns } = req.body
+  if(!fileName){
+    res.status(400).json({
+      msg: "El nombre del archivo es requerido."
+    })
+    return
+  }
+
+  if (!columns || !Array.isArray(columns) || columns.length === 0) {
+    res.status(400).json({
+      msg: "El argumento 'columns' es inválido o está vacío"
+    });
+    return;
+  }
+  
+
+  next()
+}, markFilesAsAnalyzed)
 
 export default predictRouter;
