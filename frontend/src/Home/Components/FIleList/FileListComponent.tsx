b051c2a376fce04ba0f1a911bff54e7ac4f5e08c
carlos0550 <carlospelinski03@gmail.com>
2025-04-09 22:30:57 -0300

    feat: visualizará historial de predicciones

---
+++ frontend/src/Home/Components/FIleList/FileListComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useAppContext } from '../../../Context/AppContext'
+
+function FileListComponent() {
+  
+  return (
+    <div>
+      <h3>Registro de predicciones</h3>
+      <p>Por el momento no hay ningún registro.</p>
+    </div>
+  )
+}
+
+export default FileListComponent
