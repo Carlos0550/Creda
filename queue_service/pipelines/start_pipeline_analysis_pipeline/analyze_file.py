787f30e7fcf59bbcc4974964430190c5e76a109c
carlos0550 <carlospelinski03@gmail.com>
2025-04-09 22:33:08 -0300

    feat: Agrega nuevos datos para enviar como analisis

--- queue_service/pipelines/start_pipeline_analysis_pipeline/analyze_file.py
+++ queue_service/pipelines/start_pipeline_analysis_pipeline/analyze_file.py
@@ -1,11 +1,20 @@
 import pandas as pd
+import logging
 
+logger = logging.getLogger(__name__)
 def file_analysis(file_path):
     try:
         df = pd.read_csv(file_path)
-        columns = df.columns.to_list()
-        print(columns)
-        return columns
+        
+        
+        records = {
+            "columns": df.columns.to_list(),
+            "columns_count": len(df.columns),
+            "sample_data": df.head(3).to_dict(orient="records"),
+            "null_summary":  df.isnull().sum().to_dict()
+        }
+
+        return records
     except Exception as e:
-        print(f"❌ Error al analizar el archivo {file_path}: {e}")
+        logger.debug(f"❌ Error al analizar el archivo {file_path}: {e}")
         return []
