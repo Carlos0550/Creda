4b0432c7a3761af0269dcd52840427b921649b5f
carlos0550 <carlospelinski03@gmail.com>
2025-04-09 22:30:13 -0300

    feat: Agrega Skeleton, y verificacion inicial de archivos pendientes

--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import "./Home.css"
 import FileUploader from './Components/FIleUploader/FileUploader'
+import { useAppContext } from '../Context/AppContext'
+
+import { Flex, Skeleton } from '@mantine/core'
+import FileListComponent from './Components/FIleList/FileListComponent'
 function Home() {
+  const {   
+    usePredictHook: {
+      gettingPendingFiles, verifyPendingFilesForUser, userId
+    }
+  } = useAppContext()
+
+  const alreadyFetched = useRef(false)
+  useEffect(()=>{
+    if(!userId || alreadyFetched.current) return;
+    verifyPendingFilesForUser()
+    alreadyFetched.current = true
+  },[userId])
   return (
     <React.Fragment>
         <div className='home-container'>
           <div className='operations-register-container'>
-              <h3>Registro de predicciones</h3>
-              <p>Por el momento no hay ningún registro.</p>
+              {!gettingPendingFiles && (
+                <FileListComponent/>
+              )}
+              {gettingPendingFiles && (
+                <Flex direction={"column"} gap={20} mt={10} mb={10} justify={"center"} align={"flex-start"}>
+                  <Skeleton height={15} width={200} animate/>
+                  <Skeleton height={15} width={400} animate/>
+                  <Skeleton height={15} width={400} animate/>
+                  <Skeleton height={15} width={400} animate/>
+              </Flex>
+              )}
           </div>
 
           <div className='new-operation-container'>
