3f277c6bf8ac430582ea8d0abb485f5d1dbea68c
carlos0550 <carlospelinski03@gmail.com>
2025-04-09 22:33:34 -0300

    refactor: agrega logging para debug

--- queue_service/pipelines/start_pipeline_analysis_pipeline/probeFiles.py
+++ queue_service/pipelines/start_pipeline_analysis_pipeline/probeFiles.py
@@ -1,27 +1,29 @@
 from config import backend_url
 import requests
+import logging
+logger = logging.getLogger(__name__)
 
 def get_pending_files():
     try:
-        print(f"Consultando pendientes en: {backend_url}/predict/file-status")
+        logger.debug(f"Consultando pendientes en: {backend_url}/predict/file-status")
         response = requests.get(f"{backend_url}/predict/file-status", params={"status": "pending"})
 
         if response.status_code == 404:
-            print("No hay archivos pendientes. Continuando con archivos bloqueados...")
+            logger.debug("No hay archivos pendientes. Continuando con archivos bloqueados...")
         else:
             response.raise_for_status()
             data = response.json()
             archivos_pendientes = data.get("archivos", [])
             if archivos_pendientes:
-                print("Archivos pendientes encontrados, bloqueando...")
+                logger.debug("Archivos pendientes encontrados, bloqueando...")
                 lock = requests.post(f"{backend_url}/predict/lock-pending")
                 lock.raise_for_status()
 
     except requests.RequestException as e:
-        print(f"Error consultando pendientes: {e}")
+        logger.debug(f"Error consultando pendientes: {e}")
 
     try:
-        print(f"Consultando bloqueados en: {backend_url}/predict/file-status")
+        logger.debug(f"Consultando bloqueados en: {backend_url}/predict/file-status")
         response_locked = requests.get(f"{backend_url}/predict/file-status", params={"status": "locked"})
         response_locked.raise_for_status()
         data_locked = response_locked.json()
@@ -29,5 +31,5 @@ def get_pending_files():
         return archivos_bloqueados
 
     except requests.RequestException as e:
-        print(f"❌ Error consultando archivos bloqueados: {e}")
+        logger.debug(f"❌ Error consultando archivos bloqueados: {e}")
         return []
