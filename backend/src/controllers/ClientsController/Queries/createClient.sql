30683add1aeb2ae70d274c82b5b37c1e5f31a933
carlos0550 <carlospelinski03@gmail.com>
2025-05-09 10:45:30 -0300

    Feat: SQLs de clientes

--- backend/src/controllers/ClientsController/Queries/createClient.sql
+++ backend/src/controllers/ClientsController/Queries/createClient.sql
@@ -1,11 +1,11 @@
 SELECT COUNT(*) FROM clients WHERE client_id = $1;
 
 INSERT INTO clients(
-    client_name,
-    client_nationality,
-    client_id
+    client_id,
+    client_credit_scoring,
+    client_credit_status
 )VALUES(
     $1,
     $2,
     $3
-) RETURNING client_id;
+);
