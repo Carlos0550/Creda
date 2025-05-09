47bd29e519370ea4ebe569a529c3c9ca506f4a69
Adrian31BT <adrian31baciliot@gmail.com>
2025-05-09 01:25:51 -0500

    Autenticaciones del Front-End usando API actualizada

--- frontend/src/Context/APIs.ts
+++ frontend/src/Context/APIs.ts
@@ -1,4 +1,4 @@
-export const base_url_server = new URL("http://localhost:5000/api")
+export const base_url_server = new URL("https://creda-development.up.railway.app/api")
 export const globalApis = {
     users: new URL(base_url_server + "/managers"),
     /* predict: new URL(base_url_server + "predict"), */
