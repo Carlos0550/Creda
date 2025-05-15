012169ef12487da04cadc0304a7257bf8182cdf6
carlos0550 <carlospelinski03@gmail.com>
2025-05-15 12:43:28 -0300

    Feat: define interfazes para guardado en redis

---
+++ backend/src/Types/redis.types.ts
@@ -0,0 +1,11 @@
+export interface RedisFileMetadata {
+    file_name: string; 
+    file_path: string; 
+    file_type: string; 
+    file_size: string; 
+    created_at: string;
+}
+
+export interface FileMetadataWithKey extends RedisFileMetadata {
+    key: string;
+}
