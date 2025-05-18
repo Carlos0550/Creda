ec49bd980cb557c8f28f11a761138f0cb66c5c5e
carlos0550 <carlospelinski03@gmail.com>
2025-05-17 21:42:05 -0300

    Feat: implementacion de guardado de predicts. - cambios menores

--- backend/src/routes/predict.routes.ts
+++ backend/src/routes/predict.routes.ts
@@ -3,6 +3,7 @@ import redis from "../connections/redis_conn";
 import { PredictionResult, SelectedFeatures } from "../Types/prediction.types";
 import dayjs from "dayjs";
 import { get } from "http";
+import { pipeline } from "stream";
 
 const predictRoutes = Router();
 
@@ -62,6 +63,8 @@ const StartPredictionRouter: RequestHandler<{}, {}, SelectedFeatures, {}> = asyn
             created_at: dayjs().format('YYYY-MM-DD HH:mm:ss')
         };
         await redis.hset(redisKey, predictionData);
+
+        await redis.expire(redisKey, 600); //Limite de 10 minutos
         res.status(200).json({
             msg: "Prediccion puesta en cola con exito.",
             prediction_id: key
@@ -102,7 +105,8 @@ const getPredictionStatus: RequestHandler<{}, {}, {}, { prediction_id: string }>
         };
         res.status(200).json({
             msg: "El estado de la predicción es: " + customMessage[predictionStatus] + ".",
-            status: predictionStatus
+            status: predictionStatus,
+            prediction_data: predictionStatus === "completed" ? predictionData : []
         })
     } catch (error) {
         console.error(error);
@@ -112,49 +116,51 @@ const getPredictionStatus: RequestHandler<{}, {}, {}, { prediction_id: string }>
     }
 }
 
-interface PendingPredictionInfo {
-    prediction_id: string;
-    createdAt: string | null;
+interface PredictionRedisData {
+    status: string; 
+    created_at: string; 
+    [key: string]: string | null | undefined; 
 }
+
+interface PendingPredictionsMap {
+    [redisKey: string]: PredictionRedisData;
+}
+
+
 const getPendingPredictions: RequestHandler<{}, {}, {}, {}> = async (
     req,
     res
 ): Promise<void> => {
-    const pendingPredictions: PendingPredictionInfo[] = [];
+    const pendingPredictions: PendingPredictionsMap = {};
     let cursor = '0';
 
     try {
         do {
             const scanResult = await redis.scan(cursor, 'MATCH', 'prediction:*', 'COUNT', 100);
 
-            cursor = scanResult[0];
-            const keys = scanResult[1];
+            cursor = scanResult[0]; 
+            const keys = scanResult[1]; 
 
             if (keys.length > 0) {
                 const pipeline = redis.pipeline();
                 keys.forEach(key => {
-                    pipeline.hmget(key, 'status', 'created_at');
+                    pipeline.hgetall(key);
                 });
                 const results = await pipeline.exec();
 
                 results!.forEach((result, index) => {
-                    const error = result[0];
-                    const values = result[1] as (string | null)[];
+                    const error = result[0]; 
+                    const predictionData = result[1] as PredictionRedisData | null;
 
-                    if (!error && values && values[0] === 'pending') {
+                    if (!error && predictionData && predictionData.status === 'pending') {
                         const fullKey = keys[index];
-                        const onlyKey = fullKey.split(":")[1];
-                        const createdAtValue = values[1]; 
 
-                        pendingPredictions.push({
-                            prediction_id: onlyKey,
-                            createdAt: createdAtValue
-                        });
+                        pendingPredictions[fullKey.split(":")[1]] = predictionData;
                     }
                 });
             }
 
-        } while (cursor !== '0');
+        } while (cursor !== '0'); 
         res.status(200).json(pendingPredictions);
 
     } catch (error) {
@@ -165,10 +171,6 @@ const getPendingPredictions: RequestHandler<{}, {}, {}, {}> = async (
     }
 };
 
-
-
-
-
 const SavePrediction: RequestHandler<{}, {}, PredictionResult, {prediction_result?: "completed" | "failed"}> = async (
     req,
     res
@@ -177,7 +179,8 @@ const SavePrediction: RequestHandler<{}, {}, PredictionResult, {prediction_resul
     const requiredFields: (keyof PredictionResult)[] = [
         "prediction_id",
         "client_score",
-        "client_credit_status"
+        "client_credit_status",
+        "prediction_result" 
     ]
     const missingFields = requiredFields.filter(
         (key) => body[key] === undefined || body[key] === null || body[key] === ""
@@ -189,10 +192,47 @@ const SavePrediction: RequestHandler<{}, {}, PredictionResult, {prediction_resul
         });
         return;
     }
+    const {
+        prediction_id,
+        client_score,
+        client_credit_status,
+        prediction_result
+    } = body
+    try {
+        console.log(body)
+        const redisKey = `prediction:${prediction_id}`;
+        const predictionData = await redis.hgetall(redisKey)
+        const created_at = predictionData.created_at
+        const prediction_status = prediction_result === "completed" ? "completed" : "failed";
+        const redisPipeline = redis.pipeline();
+        
+        const newDataToSave = {
+            "client_score": client_score ? client_score : 0,
+            "client_credit_status": client_credit_status ? client_credit_status : "none",
+            "prediction_result": prediction_result,
+            "status": prediction_status,
+            "created_at": created_at
+        }
+
+        redisPipeline.del(redisKey)
+        redisPipeline.hset(redisKey, newDataToSave)
+        redisPipeline.expire(redisKey, 3600)
+        await redisPipeline.exec()
+
+        res.status(200).json({
+            msg: "Prediccion guardada con exito."
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            msg: "Error al procesar y guardar los resultados de la predicción."
+        });
+    }   
 }
 
 predictRoutes.post("/start-prediction", StartPredictionRouter);
 predictRoutes.get("/prediction-status", getPredictionStatus)
 predictRoutes.get("/get-pending-predictions", getPendingPredictions)
+predictRoutes.post("/save-prediction", SavePrediction)
 
 export default predictRoutes
